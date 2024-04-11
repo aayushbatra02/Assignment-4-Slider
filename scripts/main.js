5303ce0a00f39b1ba94603b61a75d3a624438707
Aayush Batra <aayushbatra@innotechteam.in>
2024-04-11 13:09:55 +0530

    feat: add animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,94 @@
-const image = document.getElementById("image");
+const slider = document.getElementById("slider");
 const leftArrow = document.getElementById("left-arrow");
 const rightArrow = document.getElementById("right-arrow");
-const navBtn = document.querySelectorAll(".nav-btn");
+const images = document.querySelectorAll(".image");
+const buttons = document.querySelectorAll(".button");
 
-let selectedImage = 0;
+const sliderWidth = slider.offsetWidth;
+let imageNumber = 1;
 
-const disableButton = () => {
-  console.log(selectedImage);
-  if (selectedImage === 0) {
-    leftArrow.classList.add("disable");
-  } else {
-    leftArrow.classList.remove("disable");
-  }
-
-  if (selectedImage === 8) {
-    rightArrow.classList.add("disable");
-  } else {
-    rightArrow.classList.remove("disable");
-  }
-};
-
-const nextImage = () => {
-  if (selectedImage < 8) {
-    selectedImage++;
-    image.setAttribute("src", `./images/slider/image-${selectedImage}.jpg`);
+rightArrow.addEventListener("click", () => {
+  if (imageNumber < images.length) {
+    slider.style.transform = `translateX(-${sliderWidth * imageNumber}px)`;
+    imageNumber++;
+    disableArrowButton();
     changeActiveButton();
   }
-  disableButton();
-};
+});
 
-const prevImage = () => {
-  if (selectedImage > 0) {
-    selectedImage--;
-    image.setAttribute("src", `./images/slider/image-${selectedImage}.jpg`);
+leftArrow.addEventListener("click", () => {
+  if (imageNumber > 1) {
+    slider.style.transform = `translateX(-${
+      sliderWidth * (imageNumber - 2)
+    }px)`;
+    imageNumber--;
+    disableArrowButton();
     changeActiveButton();
   }
-  disableButton();
+});
+
+const disableArrowButton = () => {
+  if (imageNumber === 9) {
+    rightArrow.style.opacity = 0.3;
+  } else {
+    rightArrow.style.opacity = 1;
+  }
+
+  if (imageNumber === 1) {
+    leftArrow.style.opacity = 0.3;
+  } else {
+    leftArrow.style.opacity = 1;
+  }
 };
 
 const changeActiveButton = () => {
-  for (let i = 0; i < navBtn.length; i++) {
-    if (i === selectedImage) {
-      navBtn[i].classList.add("active");
+  buttons.forEach((button, i) => {
+    if (i === imageNumber - 1) {
+      button.style.backgroundColor = "#017AFF";
     } else {
-      navBtn[i].classList.remove("active");
+      button.style.backgroundColor = "grey";
     }
-  }
+  });
 };
 
-const navigateToImage = () => {};
+let sliderInterval;
 
-//adding click on nav buttons
-for (let i = 0; i < navBtn.length; i++) {
-  navBtn[i].addEventListener("click", () => {
-    selectedImage = i;
-    image.setAttribute("src", `./images/slider/image-${i}.jpg`);
+const startSlider = () => {
+  sliderInterval = setInterval(() => {
+    if (imageNumber < images.length) {
+      slider.style.transform = `translateX(-${sliderWidth * imageNumber}px)`;
+      imageNumber++;
+      disableArrowButton();
+      changeActiveButton();
+    } else {
+      slider.style.transform = `translateX(0px)`;
+      imageNumber = 1;
+      disableArrowButton();
+      changeActiveButton();
+    }
+  }, 2000);
+};
+
+startSlider();
+
+const stopSlider = () => {
+  clearInterval(sliderInterval);
+};
+
+slider.addEventListener("mouseover", stopSlider);
+slider.addEventListener("mouseout", startSlider);
+rightArrow.addEventListener("mouseover", stopSlider);
+rightArrow.addEventListener("mouseout", startSlider);
+leftArrow.addEventListener("mouseover", stopSlider);
+leftArrow.addEventListener("mouseout", startSlider);
+
+buttons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    slider.style.transform = `translateX(-${sliderWidth * i}px)`;
+    imageNumber = i + 1;
+    disableArrowButton();
     changeActiveButton();
-    disableButton();
   });
-}
-
-rightArrow.addEventListener("click", nextImage);
-leftArrow.addEventListener("click", prevImage);
+  button.addEventListener("mouseover", stopSlider);
+  button.addEventListener("mouseout", startSlider);
+});
